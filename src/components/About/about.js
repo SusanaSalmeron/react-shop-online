729ac019ab1a267c9a7395dc69718afae5e5498b
SusanaSalmeron <exdream76@gmail.com>
2022-06-30 12:50:12 +0200

    adjusting margins

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -3,16 +3,18 @@ import ReturnButton from '../ReturnButton/returnButton';
 
 export default function About() {
     return (
-        <div>
+        <div className={style.container}>
             <div className={style.button}>
                 <ReturnButton />
             </div>
-            <div className={style.container}>
+            <div className={style.text}>
                 <p>We are a beauty online shop.</p>
-                <p> Beauty Store was born in Madrid in 2002 to make happy people who loves make up, perfumes and cosmetics.</p>
-                <p>Our web have a wide variety of products, all of them great quality.</p>
-                <p>Every day we work to bring more products and brands.</p>
+                <p>Beauty Store was born in Madrid in 2002 to make happy people who loves make up, perfumes and cosmetics.
+                    Our web have a wide variety of products, all of them great quality.
+                    Every day we work to bring more products and brands.</p>
+                <span>XOXO</span>
             </div>
+
         </div>
     )
 }
